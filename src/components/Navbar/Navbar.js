603c4b104e3d5a91735c32ec194abfc54a173d01
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-12-17 15:40:13 +0800

    Add sample image in body

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,36 +6,32 @@ import sjccLogo from '../../img/logo.png'
 
 function Navbar() {
     return (
-        <div className="container">
-            <nav id="nav" className="navbar navbar-expand-md bg-color-1 navbar-light">
-                <div className="container">
-                    <div class="d-flex align-items-center justify-content-between w-100">
-                        {/* <div className="d-none d-md-block"> */}
-                            <ul class="nav justify-content-center mb-0">
-                                <li><a href="#" className="text-color-2 link-hover-1 nav-link">UPDATES</a></li>
-                                <li><a href="#" className="text-color-2 link-hover-1 nav-link">ALUMNI</a></li>
-                                <li><a href="#" className="text-color-2 link-hover-1 nav-link">GALLERY</a></li>
-                                <li><a href="#" className="text-color-2 link-hover-1 nav-link">CAREER</a></li>
-                            </ul>
-                        {/* </div> */}
-                        <div class="d-flex align-items-center">
-                            <Link exact="true" to="/" className="navbar-brand">
-                                <img src={sjccLogo} className="d-inline-block align-top" alt="SJCC Logo" height="65px" width="auto" /> 
-                            </Link>
-                            <p className="mb-0">San Jose Community College</p>
-                        </div>
-                        <div className="d-none d-md-block">
-                            <ul class="nav justify-content-center mb-0">
-                                <li><a href="#" className="text-color-2 link-hover-1 nav-link">ABOUT SJCC</a></li>
-                                <li><a href="#" className="text-color-2 link-hover-1 nav-link">ADMISSION</a></li>
-                                <li><a href="#" className="text-color-2 link-hover-1 nav-link">RESOURCES</a></li>
-                                <li><a href="#" className="text-color-2 link-hover-1 nav-link">CONTACT US</a></li>
-                            </ul>
-                        </div>
-                    </div>
+        <nav id="nav" className="navbar navbar-expand-md bg-color-1 navbar-light">
+            <div class="d-flex align-items-center justify-content-evenly w-100">
+                {/* <div className="d-none d-md-block"> */}
+                    <ul class="nav justify-content-center mb-0">
+                        <li><a href="#" className="text-color-2 link-hover-1 nav-link">UPDATES</a></li>
+                        <li><a href="#" className="text-color-2 link-hover-1 nav-link">ALUMNI</a></li>
+                        <li><a href="#" className="text-color-2 link-hover-1 nav-link">GALLERY</a></li>
+                        <li><a href="#" className="text-color-2 link-hover-1 nav-link">CAREER</a></li>
+                    </ul>
+                {/* </div> */}
+                <div class="d-flex align-items-center">
+                    <Link exact="true" to="/" className="navbar-brand">
+                        <img src={sjccLogo} className="d-inline-block align-top" alt="SJCC Logo" height="65px" width="auto" /> 
+                    </Link>
+                    <p className="mb-0">San Jose Community College</p>
                 </div>
-            </nav>
-        </div>
+                <div className="d-none d-md-block">
+                    <ul class="nav justify-content-center mb-0">
+                        <li><a href="#" className="text-color-2 link-hover-1 nav-link">ABOUT SJCC</a></li>
+                        <li><a href="#" className="text-color-2 link-hover-1 nav-link">ADMISSION</a></li>
+                        <li><a href="#" className="text-color-2 link-hover-1 nav-link">RESOURCES</a></li>
+                        <li><a href="#" className="text-color-2 link-hover-1 nav-link">CONTACT US</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
